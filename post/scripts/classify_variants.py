25537e7d35714f3579f65518d87a928298611139
Romain Durand <duran2101@gmail.com>
2026-01-07 14:06:51 -0500

    update classification

--- post/scripts/classify_variants.py
+++ post/scripts/classify_variants.py
@@ -32,7 +32,11 @@ def main():
     classified_outpath = f'../classified/{"_".join([args.strain,args.locus])}/{args.o}'
 
     # Import data
-    df = pd.read_csv(data_path).rename(columns={"fitness_T2": "s"})
+    fulldf = pd.read_csv(data_path).rename(columns={"fitness_T2": "s", "Mutated_seq": "locus"})
+    if ("strain" in fulldf.columns) & ("locus" in fulldf.columns):
+        df = fulldf[(fulldf.strain == args.strain) & (fulldf.locus == args.locus)].copy()
+    else: # assume single strain, single locus and file is located in strain-locus-specific folder
+        df = fulldf.copy()
     stddf = pd.read_csv(thresholds)
 
     # Apply thresholds
